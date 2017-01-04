27b5d62c32a30b71c542e6bc26626594e4f91762
Murilo Moreira <murilo.dmoreira@gmail.com>
2017-01-04 18:21:02 -0200

    Modified files

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+var sideSize = 16;
+var pix = 700;
+
 $(document).ready(function(){
-	$(".TestSquare").fadeOut(1600, complete);
+	gridInit(sideSize);
+	draw();
+
+	$('button').click(function(){
+		clearGrid();
+		sideSize=prompt("How many sides on the new grid?");
+		console.log(sideSize);
+		gridInit();
+		draw();
+	});
 });
+
+
+function gridInit(size){
+	for(var i=0; i<size*size; i++){
+			$("#wrapper").append($('<div class="grid"></div>'));
+	}
+}
+
+function clearGrid(){
+	$("div.grid").remove();
+	console.log("cleared");
+}
+
+function draw(){
+	$("div.grid").hover(function(){
+		$(this).addClass('touch');
+		$("div.touch").css("opacity", "+=0.5");
+	});
+}
